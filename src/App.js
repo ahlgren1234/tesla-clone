49f5c631a0a09e8f65d7c5c8470ac7a8a1601cfa
Peter Ahlgren <ahlgren1234@gmail.com>
2021-02-11 14:26:46 +0100

    Header + assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Heloooo</h1>
+      <Header />
     </div>
   );
 }
